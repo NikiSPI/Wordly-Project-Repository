cf2528401e2e4c33c710bd75860fceea6550042f
NikiSPI <nicola.bozhkov@gmail.com>
2023-10-10 12:16:20 +0300

    Made a Form

--- Program.cs
+++ Program.cs
@@ -1,8 +1,25 @@
-namespace WordTester;
+
+using System.Diagnostics;
+
+public class Frame : Form
+{
+    StackFrame stackFrame = new StackTrace(new StackFrame(true)).GetFrame(0);
+    
+    public Frame()
+    {
+
+        Text = " Word Tester";
+        Icon = Icon.ExtractAssociatedIcon(Path.GetDirectoryName(stackFrame.GetFileName()) + "\\Images\\WordTester.ico");
+        ClientSize = new Size(300, 300);
+
+    }
+    
+}
 
 static class Program
 {
     static void Main()
     {
+        Application.Run(new Frame());
     }
 }
